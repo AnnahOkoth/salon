c1aacafc43e8d95421a6fdc105c05509a24ccf3a
AnnahOkoth <Annah.Okoth@riarauniversity.ac.ke>
2023-07-01 21:36:19 +0300

    jjjj

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import './App.css';
+import React from 'react';
 import Navbar from './Components/Navbar';
 import Home from './Components/Home';
 
-
 function App() {
   return (
-    <div className="ml-[5%] mr-[5%] flex flex-col overflow-hidden gap-5 justify-center">
-      <Navbar />
-      <Home />
-    </div>
+      <div className="ml-[5%] mr-[5%] flex flex-col overflow-hidden gap-5 justify-center">
+        <Navbar />
+        <Home />
+      </div>
   );
 }
 
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,26 +1,27 @@
 import React from 'react';
-
-
+import { Link } from 'react-router-dom';
 
 function Hero() {
   return (
-    <div className="flex flex-col lg:flex-row text-green-800 rounded-sm bg-red-100  items-center justify-evenly min-h-screen ">
+    <div className="flex flex-col lg:flex-row text-green-800 rounded-sm bg-red-100 items-center justify-evenly min-h-screen">
       <div className="m-4 flex flex-col gap-4">
         <h1 className="text-3xl lg:text-6xl font-bold leading-tight text-center">
-          We Care About 
+          We Care About
           <br />
           Your Hair
         </h1>
-        <p className=" leading-relaxed text-center line-clamp-4">
+        <p className="leading-relaxed text-center line-clamp-4">
           "Discover your own style. Don't try to repeat what has already been
           written - have the courage to do your own thing and don't be afraid to
           do something different."
         </p>
-       <div className="flex justify-center">
-       <button className="bg-blue-500 p-3 text-white w-32 rounded-full">
-          About Us
-        </button>
-       </div>
+        <div className="flex justify-center">
+          <Link to="/aboutus">
+            <button className="bg-blue-500 p-3 text-white w-32 rounded-full">
+              About Us
+            </button>
+          </Link>
+        </div>
       </div>
       <div>
         <img className="hidden lg:block" src="pod.png" alt="Pod" />
@@ -30,3 +31,4 @@ function Hero() {
 }
 
 export default Hero;
+
